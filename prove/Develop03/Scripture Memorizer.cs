acc59160e41628352295809874aa6d96a19af507
Gitboy0123 <144740287+Gitboy0123@users.noreply.github.com>
2023-10-04 12:24:33 -0600

    Update Scripture Memorizer.cs

--- prove/Develop03/Scripture Memorizer.cs
+++ prove/Develop03/Scripture Memorizer.cs
@@ -6,19 +6,15 @@ class Program
 {
     static void Main()
     {
-
         Scripture scripture = new Scripture("John 3:16", "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.");
-
 
         Console.Clear();
         scripture.DisplayScripture();
-
 
         while (true)
         {
             Console.WriteLine("Press enter to hide a word or type quit to exit:");
             string userInput = Console.ReadLine();
-
 
             if (userInput.ToLower() == "quit")
             {
@@ -26,11 +22,9 @@ class Program
             }
             else
             {
-
                 Console.Clear();
                 scripture.HideWord();
                 scripture.DisplayScripture();
-
 
                 if (scripture.AllWordsHidden())
                 {
@@ -44,32 +38,27 @@ class Program
 
 class Scripture
 {
-    private string reference;
-    private string text;
-    private List<string> hiddenWords;
+    private Reference reference;
+    private List<Word> words;
 
     public Scripture(string reference, string text)
     {
-        this.reference = reference;
-        this.text = text;
-        this.hiddenWords = new List<string>();
+        this.reference = new Reference(reference);
+        this.words = text.Split(' ').Select(wordText => new Word(wordText)).ToList();
     }
 
     public void DisplayScripture()
     {
-
-        Console.WriteLine(reference);
-        string[] words = text.Split(' ');
-        foreach (string word in words)
+        Console.WriteLine(reference.ToString());
+        foreach (Word word in words)
         {
-
-            if (hiddenWords.Contains(word))
+            if (word.IsHidden)
             {
                 Console.Write("_____ ");
             }
             else
             {
-                Console.Write(word + " ");
+                Console.Write(word.Text + " ");
             }
         }
         Console.WriteLine();
@@ -77,20 +66,44 @@ class Scripture
 
     public void HideWord()
     {
-
-        string[] words = text.Split(' ');
-        Random rand = new Random();
-        int index = rand.Next(words.Length);
-        string wordToHide = words[index];
-
-
-        hiddenWords.Add(wordToHide);
+        List<Word> visibleWords = words.Where(word => !word.IsHidden).ToList();
+        if (visibleWords.Count > 0)
+        {
+            Random rand = new Random();
+            int index = rand.Next(visibleWords.Count);
+            visibleWords[index].IsHidden = true;
+        }
     }
 
     public bool AllWordsHidden()
     {
+        return words.All(word => word.IsHidden);
+    }
+}
 
-        string[] words = text.Split(' ');
-        return hiddenWords.Count == words.Length;
+class Reference
+{
+    public string Text { get; private set; }
+
+    public Reference(string reference)
+    {
+        Text = reference;
+    }
+
+    public override string ToString()
+    {
+        return Text;
+    }
+}
+
+class Word
+{
+    public string Text { get; private set; }
+    public bool IsHidden { get; set; }
+
+    public Word(string text)
+    {
+        Text = text;
+        IsHidden = false;
     }
 }
